79afbd2a5a2256e5e5732b012991821a6266deab
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-30 09:45:51 -0300

    o famoso ph

--- api/db.js
+++ api/db.js
@@ -4,7 +4,7 @@ export const db = mysql.createConnection({
     host: "localhost",
     user:"root",
     password:"",
-    database:"db_usuario",
+    database:"db_usuario", // colocar o nome do seu banco
     
 });
 
